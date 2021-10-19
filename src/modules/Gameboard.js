462a3fe0738c6ca566fb814247d90e88f0dff35c
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-10-19 21:35:10 +0500

    Add gameboardArray, shipsOnTheBoard getters & add method

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -1,3 +1,25 @@
-const Gameboard = () => {};
+const Gameboard = () => {
+  const gameboardArray = Array(10).fill(Array(10).fill(""));
+  let shipsOnTheBoard = [];
+  const add = (ship) => {
+    shipsOnTheBoard = [...shipsOnTheBoard, ship];
+    return {
+      at(x, y) {
+        const column = gameboardArray[x].slice();
+        column.fill(ship.name, y, y + ship.length);
+        gameboardArray[x] = column;
+      },
+    };
+  };
+  return {
+    get gameboardArray() {
+      return [...gameboardArray];
+    },
+    get shipsOnTheBoard() {
+      return [...shipsOnTheBoard];
+    },
+    add,
+  };
+};
 
 export default Gameboard;
