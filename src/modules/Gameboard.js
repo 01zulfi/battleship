df7ef2d85a55313b5fa9c213ab7e6619fcb8ac2d
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-10-20 18:59:50 +0500

    Add checkIfShipPresent() & removeShipFromBoard()

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -1,13 +1,26 @@
 const Gameboard = () => {
   const gameboardArray = Array(10).fill(Array(10).fill(""));
   let shipsOnTheBoard = [];
+  const checkIfShipPresent = (x, y, shipLength) => {
+    const requiredArray = [...gameboardArray[x]].slice(y, y + shipLength);
+    return [...requiredArray].some((element) => element !== "");
+  };
+  const removeShipFromBoard = (ship) => {
+    shipsOnTheBoard = [...shipsOnTheBoard].filter(
+      (element) => element !== ship,
+    );
+  };
   const add = (ship) => {
     shipsOnTheBoard = [...shipsOnTheBoard, ship];
     return {
       at(x, y) {
-        const column = gameboardArray[x].slice();
+        if (checkIfShipPresent(x, y, ship.length)) {
+          removeShipFromBoard(ship);
+          return;
+        }
+        const column = [...gameboardArray[x]];
         column.fill(ship.name, y, y + ship.length);
-        gameboardArray[x] = column;
+        gameboardArray[x] = [...column];
       },
     };
   };
