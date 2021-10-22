b7d434bcf00e8436aebba0b30ece60900cf625e1
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-10-22 18:52:50 +0500

    Fix bug in attackShip()

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -23,19 +23,13 @@ const Gameboard = () => {
   const attackShip = (x, y) => {
     const ship = board[x][y];
     let hitPosition = 0;
-    if (y < ship.length) {
-      // ship touches left boundary
-      hitPosition = y;
-    } else {
-      // ship has space from left boundary
-      // get left of gameboard from hit position
-      const targetArea = board[x].slice(0, y);
-      // filter to get left of ship from hit position
-      const shipLeftSide = targetArea.filter(
-        (element) => element.name === ship.name,
-      );
-      hitPosition = shipLeftSide.length;
-    }
+    // get left of gameboard from hit position
+    const targetArea = board[x].slice(0, y);
+    // filter to get left of ship from hit position
+    const shipLeftSide = targetArea.filter(
+      (element) => element.name === ship.name,
+    );
+    hitPosition = shipLeftSide.length;
     if (ship.isHitAt(hitPosition)) {
       latestAttackStatus = "illegal";
       return;
@@ -77,7 +71,7 @@ const Gameboard = () => {
       };
     },
     areAllShipsSunk() {
-      return [...shipsArray].every((ship) => ship.isSunk() === true);
+      return [...shipsArray].every((ship) => ship.isSunk());
     },
   };
 };
