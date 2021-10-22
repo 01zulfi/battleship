f1ec12581365f200c5c3d516300e24da1af86382
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-10-22 18:54:03 +0500

    Add name, fleet, attack methods & their tests

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -1,7 +1,21 @@
+import Gameboard from "./Gameboard";
+
 const Player = (name) => {
+  const fleet = Gameboard();
+
   return {
     get name() {
       return name;
+    },
+    get fleet() {
+      return fleet;
+    },
+    attack(enemy) {
+      return {
+        at(x, y) {
+          enemy.fleet.at(x, y).receiveAttack(x, y);
+        },
+      };
     },
   };
 };
--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -4,8 +4,82 @@ describe("test player", () => {
   const player = Player("player");
   const computer = Player("computer");
 
+  const ship1 = {
+    name: "firstShip",
+    length: 5,
+    hit: jest.fn((position) => {
+      ship1.shipArray[position] = "hit";
+      return "Hit";
+    }),
+    shipArray: ["", "", "", "", ""],
+    isHitAt: jest.fn((position) => {
+      return [...ship1.shipArray][position] === "hit";
+    }),
+    isSunk: jest.fn(() =>
+      [...ship1.shipArray].every((element) => element === "hit"),
+    ),
+  };
+
+  const ship2 = {
+    name: "secondShip",
+    length: 5,
+    hit: jest.fn((position) => {
+      ship2.shipArray[position] = "hit";
+      return "Hit";
+    }),
+    shipArray: ["", ""],
+    isHitAt: jest.fn((position) => {
+      return [...ship2.shipArray][position] === "hit";
+    }),
+    isSunk: jest.fn(() =>
+      [...ship2.shipArray].every((element) => element === "hit"),
+    ),
+  };
+
+  const ship3 = {
+    name: "thirdShip",
+    length: 5,
+    hit: jest.fn((position) => {
+      ship3.shipArray[position] = "hit";
+      return "Hit";
+    }),
+    shipArray: ["", ""],
+    isHitAt: jest.fn((position) => {
+      return [...ship3.shipArray][position] === "hit";
+    }),
+    isSunk: jest.fn(() =>
+      [...ship3.shipArray].every((element) => element === "hit"),
+    ),
+  };
+
+  player.fleet.at(0, 0).add(ship1);
+  player.fleet.at(3, 2).add(ship2);
+  computer.fleet.at(0, 0).add(ship3);
+
   test("returns correct player name", () => {
     expect(player.name).toMatch(/player/);
     expect(computer.name).toMatch(/computer/);
+  });
+
+  test("creates gameboards", () => {
+    expect(player).toHaveProperty("fleet");
+    expect(computer).toHaveProperty("fleet");
+  });
+
+  test("can attack each other", () => {
+    player.attack(computer).at(0, 3);
+    expect(computer.fleet.shipsArray[0].isHitAt(3)).toBe(true);
+
+    computer.attack(player).at(3, 2);
+    computer.attack(player).at(3, 3);
+    computer.attack(player).at(3, 4);
+    computer.attack(player).at(3, 5);
+    computer.attack(player).at(3, 6);
+    expect(player.fleet.shipsArray[1].isHitAt(2)).toBe(true);
+    expect(player.fleet.shipsArray[1].isHitAt(4)).toBe(true);
+    expect(player.fleet.shipsArray[1].isSunk()).toBe(true);
+
+    computer.attack(player).at(0, 1);
+    expect(player.fleet.shipsArray[0].shipArray[1]).toMatch(/hit/);
   });
 });
