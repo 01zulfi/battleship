b931410d99c0dd5320cf2ae22ccce9e9a4e6a2af
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-10-31 19:27:32 +0500

    Add favicon

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import "./styles/reset.css";
 import "./styles/normalize.css";
 import "./styles/style.css";
+import boatIcon from "./icons/boat-icon.png";
 import gameModuleObject from "./modules/Game";
 import DOMModuleObject from "./modules/DOM";
 
 DOMModuleObject.execute();
 gameModuleObject.execute();
+
+(() => {
+  const head = document.querySelector("head");
+  const link = document.createElement("link");
+  link.href = boatIcon;
+  link.type = "image/png";
+  link.rel = "icon";
+  head.append(link);
+})();
